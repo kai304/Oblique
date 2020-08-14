3ad390e09d4c639e135617c8960afcb8ed0d6116
kaiukato <kaiu.kato39@gmail.com>
2020-08-14 21:19:36 +0900

    実験環境完成

--- Assets/Scripts/BallShooter.cs
+++ Assets/Scripts/BallShooter.cs
@@ -6,24 +6,26 @@ using UnityEngine.UI;
 public class BallShooter : MonoBehaviour {
     // textオブジェクト参照用
     GameObject textObject;
-
+    float xMax = 0;
     // Textコンポーネント参照用
     Text scoreText;
-
+    public float Velocity;
+    public float angle;
     void Start () {
         // 初速, 角度
-        float INIT_SPEED = 20f;
-		float INIT_DEGREE = 60f;
+        
+        //float INIT_SPEED = 20f;
+		//float INIT_DEGREE = 60f;
 
         // Rigidbodyコンポーネントへの参照を取得
         Rigidbody rb = gameObject.GetComponent<Rigidbody>();
 
         // 初速を与える
         Vector3 vel = Vector3.zero;
-        vel.y = INIT_SPEED;
+        vel.y = Velocity;
 		
-		vel.x = INIT_SPEED*Mathf.Cos(INIT_DEGREE*Mathf.PI/180f);
-		vel.y = INIT_SPEED*Mathf.Sin(INIT_DEGREE*Mathf.PI/180f);
+		vel.x = Velocity * Mathf.Cos(angle*Mathf.PI/180f);
+		vel.y = Velocity * Mathf.Sin(angle*Mathf.PI/180f);
 		rb.velocity = vel;
 
         // Textオブジェクトの参照
@@ -35,9 +37,13 @@ public class BallShooter : MonoBehaviour {
 
     void FixedUpdate(){
         scoreText.text = "X：" + gameObject.transform.position.x.ToString();
+        if (gameObject.transform.position.y <= 0.5)
+        {
+            xMax = gameObject.transform.position.x;
+            Debug.Log(xMax);
+        }
     }
 
     void Update () {
-		
     }
 }
